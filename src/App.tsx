e909873ed77d1ce248571832d37d53d3c9e51d1a
parallel <parallelkim12@gmail.com>
2023-10-20 13:15:49 +0900

    Add: Chakra and carousel libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
 
 export const App = () => {
     return (
         <div id="app">
-            <Header />
-            <Outlet />
-            <Footer />
+            <ChakraProvider>
+                <Header />
+                <Outlet />
+                <Footer />
+            </ChakraProvider>
         </div>
     );
 };
