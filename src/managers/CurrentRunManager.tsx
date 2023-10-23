afadb3be868c031e10075d7de47a839147286755
parallel <parallelkim12@gmail.com>
2023-10-23 22:39:15 +0900

    Feat: Shuffle when init run

--- src/managers/CurrentRunManager.tsx
+++ src/managers/CurrentRunManager.tsx
@@ -3,6 +3,7 @@ import { currentCardSetAtom } from "../stores/atoms";
 import { useEffect } from "react";
 
 import chars from "../constants/chars.json";
+import { shuffleArray } from "../utils";
 
 export const CurrentRunManager = () => {
     const [currentCardSet, setCurrentCardSet] = useAtom(currentCardSetAtom);
@@ -15,10 +16,12 @@ export const CurrentRunManager = () => {
 
         const rand = Math.round(Math.random() * (chars.length - 20));
 
+        shuffleArray(chars);
+
         setCurrentCardSet(
             chars
-                .map((c, i) => ({ id: i, front: c.한자, back: c.뜻 }))
                 .slice(rand, rand + 20)
+                .map((c, i) => ({ id: i, front: c.한자, back: c.뜻 }))
         );
     };
 
