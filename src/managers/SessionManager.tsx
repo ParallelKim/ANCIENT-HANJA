b09c59514ec319f79406f5c5847b62e22cbf81e9
parallel <parallelkim12@gmail.com>
2023-10-24 22:11:44 +0900

    Comment: About user info data

--- src/managers/SessionManager.tsx
+++ src/managers/SessionManager.tsx
@@ -8,6 +8,8 @@ export const SessionManager = () => {
     const [sessionId, setSessionId] = useAtom(sessionIdAtom);
 
     const initSession = () => {
+        // 중요도: 1.구글, 2.로컬 스토리지, 3.url hash. 하나 이상의 값이 다르다면 덮어씌울지 합칠지 정하도록
+
         if (!sessionId) {
             setSessionId(uuid());
         }
