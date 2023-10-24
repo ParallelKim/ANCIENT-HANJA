ba64e723f77d8f2364790859464a7ba961ad10c7
parallel <parallelkim12@gmail.com>
2023-10-24 12:31:50 +0900

    Feat: Add keyboardListener

--- src/managers/CurrentRunManager.tsx
+++ src/managers/CurrentRunManager.tsx
@@ -1,5 +1,5 @@
-import { useAtom } from "jotai";
-import { currentCardSetAtom } from "../stores/atoms";
+import { useAtom, useSetAtom } from "jotai";
+import { currentCardSetAtom, moveCurrentIndexAtom } from "../stores/atoms";
 import { useEffect } from "react";
 
 import chars from "../constants/chars.json";
@@ -7,22 +7,41 @@ import { shuffleArray } from "../utils";
 
 export const CurrentRunManager = () => {
     const [currentCardSet, setCurrentCardSet] = useAtom(currentCardSetAtom);
+    const moveCurrentIndex = useSetAtom(moveCurrentIndexAtom);
 
     const initRun = () => {
-        if (currentCardSet.length > 0) {
-            console.log("currentCardSet", currentCardSet);
-            return;
+        if (currentCardSet.length === 0) {
+            const rand = Math.round(Math.random() * (chars.length - 20));
+
+            shuffleArray(chars);
+
+            setCurrentCardSet(
+                chars
+                    .slice(rand, rand + 20)
+                    .map((c, i) => ({ id: i, front: c.한자, back: c.뜻 }))
+            );
         }
 
-        const rand = Math.round(Math.random() * (chars.length - 20));
+        const keyboardListener = (e: KeyboardEvent) => {
+            console.log(e);
+            switch (e.code) {
+                case "Space":
+                    break;
+                case "ArrowRight":
+                    moveCurrentIndex("next");
+                    break;
+                case "ArrowLeft":
+                    moveCurrentIndex("prev");
+                    break;
+                default:
+                    break;
+            }
+        };
 
-        shuffleArray(chars);
-
-        setCurrentCardSet(
-            chars
-                .slice(rand, rand + 20)
-                .map((c, i) => ({ id: i, front: c.한자, back: c.뜻 }))
-        );
+        document.addEventListener("keydown", keyboardListener);
+        return () => {
+            document.removeEventListener("keydown", keyboardListener);
+        };
     };
 
     useEffect(initRun, [currentCardSet, setCurrentCardSet]);
