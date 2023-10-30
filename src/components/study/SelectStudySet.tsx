479a4f1c9ec3e35b71c5060d33a5952d0ede427b
parallel <parallelkim12@gmail.com>
2023-10-31 00:23:53 +0900

    Feat: Class preview

--- src/components/study/SelectStudySet.tsx
+++ src/components/study/SelectStudySet.tsx
@@ -1,11 +1,26 @@
-import { useSetAtom } from "jotai";
+import { useState } from "react";
+import { useAtom, useSetAtom } from "jotai";
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Paper,
+    Stack,
+    Typography,
+} from "@mui/material";
+import ManageSearchIcon from "@mui/icons-material/ManageSearch";
 
-import { Button, Paper, Stack } from "@mui/material";
-import { currentCardSetAtom, currentClassAtom } from "../../stores/atoms";
+import { currentClassAtom } from "../../stores/atoms";
 import chars1 from "../../assets/chars1.json";
 import chars2 from "../../assets/chars2.json";
 import words1 from "../../assets/words1.json";
 import words2 from "../../assets/words2.json";
+import { CARD } from "../../types/card";
 
 const SELECTABLE = [
     { label: "한자 1", set: chars1 },
@@ -16,25 +31,118 @@ const SELECTABLE = [
 
 export const SelectStudySet = () => {
     const setCurrentCardSet = useSetAtom(currentClassAtom);
+    const [preview, setPreview] = useState<{
+        title: string;
+        contents: CARD[];
+    } | null>(null);
 
     return (
-        <Paper>
-            <Stack>
+        <Paper sx={{ my: 2, p: 2 }}>
+            <Typography
+                mb={2}
+                variant="h5"
+                fontWeight={700}
+            >
+                학습할 코스를 선택해 주세요
+            </Typography>
+            <Stack gap={2}>
                 {SELECTABLE.map((btn) => (
-                    <Button
+                    <ButtonGroup
                         key={btn.label}
-                        onClick={() =>
-                            setCurrentCardSet(
-                                btn.set.map((card, idx) => ({
-                                    id: `${btn.label} - ${idx}`,
-                                    ...card,
-                                }))
-                            )
-                        }
+                        sx={{ justifyContent: "center" }}
                     >
-                        {btn.label}
-                    </Button>
+                        <Button
+                            variant="contained"
+                            size="large"
+                            onClick={() =>
+                                setCurrentCardSet(
+                                    btn.set.map((card, idx) => ({
+                                        id: `${btn.label} - ${idx}`,
+                                        ...card,
+                                    }))
+                                )
+                            }
+                        >
+                            {btn.label}
+                        </Button>
+                        <Button
+                            size="small"
+                            color="primary"
+                            onClick={() => {
+                                setPreview({
+                                    title: btn.label,
+                                    contents: btn.set.map((card, idx) => ({
+                                        id: `${btn.label} - ${idx}`,
+                                        ...card,
+                                    })),
+                                });
+                            }}
+                        >
+                            <ManageSearchIcon />
+                        </Button>
+                    </ButtonGroup>
                 ))}
+                <Dialog
+                    open={Boolean(preview)}
+                    onClose={() => setPreview(null)}
+                    maxWidth="xs"
+                    scroll="paper"
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle
+                        id="alert-dialog-title"
+                        variant="h4"
+                    >
+                        카드 미리보기
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            <Stack gap={2}>
+                                {preview?.contents.map((card, idx) => (
+                                    <Paper key={card.id}>
+                                        <Stack
+                                            flex={1}
+                                            px={4}
+                                            py={1}
+                                            direction="row"
+                                            gap={2}
+                                            justifyContent="space-around"
+                                        >
+                                            <Typography fontSize="2rem">
+                                                {idx + 1}
+                                            </Typography>
+                                            <Box
+                                                flex={1}
+                                                gap={2}
+                                                display="flex"
+                                                justifyContent="center"
+                                            >
+                                                <Typography fontSize="2rem">
+                                                    {card.front}
+                                                </Typography>
+                                                <Typography fontSize="2rem">
+                                                    {card.back}
+                                                </Typography>
+                                            </Box>
+                                        </Stack>
+                                    </Paper>
+                                ))}
+                            </Stack>
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => setPreview(null)}>
+                            Disagree
+                        </Button>
+                        <Button
+                            onClick={() => setPreview(null)}
+                            autoFocus
+                        >
+                            Agree
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </Stack>
         </Paper>
     );
