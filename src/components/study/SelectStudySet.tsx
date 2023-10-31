66c0c769e92f1375047301facfa8a0eb46d2b1cc
parallel <parallelkim12@gmail.com>
2023-10-31 09:01:17 +0900

    Feat: Add start study button

--- src/components/study/SelectStudySet.tsx
+++ src/components/study/SelectStudySet.tsx
@@ -96,59 +96,72 @@ export const SelectStudySet = () => {
                     >
                         카드 미리보기
                     </DialogTitle>
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            <Stack
-                                gap={2}
-                                p={2}
-                                mx={-3}
-                                sx={{ backgroundColor: "primary.dark" }}
-                            >
-                                {preview?.contents.map((card, idx) => (
-                                    <Paper key={card.id}>
-                                        <Stack
-                                            flex={1}
-                                            px={4}
-                                            py={1}
-                                            direction="row"
-                                            gap={2}
-                                            justifyContent="space-around"
-                                        >
-                                            <Typography
-                                                textAlign="right"
-                                                lineHeight="3rem"
-                                                fontSize="1.5rem"
-                                            >
-                                                {idx + 1}
-                                            </Typography>
-                                            <Box
-                                                flex={1}
-                                                gap={2}
-                                                display="flex"
-                                                justifyContent="center"
-                                            >
-                                                <Typography fontSize="2rem">
-                                                    {card.front}
-                                                </Typography>
-                                                <Typography fontSize="2rem">
-                                                    {card.back}
-                                                </Typography>
-                                            </Box>
-                                        </Stack>
-                                    </Paper>
-                                ))}
-                            </Stack>
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions sx={{ p: 2 }}>
-                        <Button
-                            size="large"
-                            variant="contained"
-                            onClick={() => setPreview(null)}
-                        >
-                            닫기
-                        </Button>
-                    </DialogActions>
+                    {preview && (
+                        <>
+                            <DialogContent>
+                                <DialogContentText id="alert-dialog-description">
+                                    <Stack
+                                        gap={2}
+                                        p={2}
+                                        mx={-3}
+                                        sx={{ backgroundColor: "primary.dark" }}
+                                    >
+                                        {preview?.contents.map((card, idx) => (
+                                            <Paper key={card.id}>
+                                                <Stack
+                                                    flex={1}
+                                                    px={4}
+                                                    py={1}
+                                                    direction="row"
+                                                    gap={2}
+                                                    justifyContent="space-around"
+                                                >
+                                                    <Typography
+                                                        textAlign="right"
+                                                        lineHeight="3rem"
+                                                        fontSize="1.5rem"
+                                                    >
+                                                        {idx + 1}
+                                                    </Typography>
+                                                    <Box
+                                                        flex={1}
+                                                        gap={2}
+                                                        display="flex"
+                                                        justifyContent="center"
+                                                    >
+                                                        <Typography fontSize="2rem">
+                                                            {card.front}
+                                                        </Typography>
+                                                        <Typography fontSize="2rem">
+                                                            {card.back}
+                                                        </Typography>
+                                                    </Box>
+                                                </Stack>
+                                            </Paper>
+                                        ))}
+                                    </Stack>
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions sx={{ p: 2 }}>
+                                <Button
+                                    size="large"
+                                    variant="contained"
+                                    onClick={() =>
+                                        setCurrentCardSet(preview?.contents)
+                                    }
+                                >
+                                    학습 시작
+                                </Button>
+                                <Button
+                                    size="large"
+                                    variant="contained"
+                                    onClick={() => setPreview(null)}
+                                >
+                                    닫기
+                                </Button>
+                            </DialogActions>
+                        </>
+                    )}
                 </Dialog>
             </Stack>
         </Paper>
