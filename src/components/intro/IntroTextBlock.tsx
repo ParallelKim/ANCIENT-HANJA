23291248479becaaa728b3094163f8d44ff26486
parallel <parallelkim12@gmail.com>
2023-11-05 00:29:23 +0900

    Update: Structure of introduce page

--- src/components/intro/IntroTextBlock.tsx
+++ src/components/intro/IntroTextBlock.tsx
@@ -4,12 +4,16 @@ export const IntroTextBlock = () => {
   return (
     <Box>
       <Paper sx={{ p: 2, fontSize: "2rem" }}>
-        <Typography>안녕하세요</Typography>
-        <Typography>한자 공부 웹을 만들었습니다</Typography>
+        <Typography variant="h5" fontWeight={700}>
+          고대한자(베타)
+        </Typography>
+        <Typography variant="h6">업데이트 내역</Typography>
+        <Typography>1. 일정 알림 기능 추가</Typography>
+        <Typography>2. 기출 문제 별 데이터 추가</Typography>
+        <Typography>3. 소소한 디자인 업데이트</Typography>
         <p />
-        <Typography>아직 기능이 많지는 않지만, 천천히 업데이트 해 나갈 예정입니다</Typography>
-        <Typography>아래에 제게 연락할 수 있는 링크가 있습니다</Typography>
-        <Typography>기능 요청, 버그 제보, 기여, 커피챗 등 모든 문의 환영입니다</Typography>
+        <Typography>기능 요청, 버그 제보, 팀 참여 희망, 커피챗 등</Typography>
+        <Typography>모든 문의 환영입니다 편하게 연락 주세요</Typography>
         <p />
         <Typography>다들 한자 시험 파이팅입니다</Typography>
       </Paper>
