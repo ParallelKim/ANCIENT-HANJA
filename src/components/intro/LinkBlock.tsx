d7b2b6d9bd3163cef138ca6d6e8d2982886f80b3
parallel <parallelkim12@gmail.com>
2023-11-07 22:21:46 +0900

    Refactor: Links in introduce

--- src/components/intro/LinkBlock.tsx
+++ src/components/intro/LinkBlock.tsx
@@ -4,22 +4,38 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import { ReactComponent as KakaoTalkIcon } from "../../assets/kakaoIcon.svg";
 import { GITHUB, INSTAGRAM, OPEN_KAKAO } from "../../constants/externalURL";
 
+const LINKS = [
+  {
+    label: "kakao-icon",
+    href: OPEN_KAKAO,
+    icon: (
+      <SvgIcon fontSize="large">
+        <KakaoTalkIcon />
+      </SvgIcon>
+    ),
+  },
+  {
+    label: "instagram-icon",
+    href: INSTAGRAM,
+    icon: <InstagramIcon fontSize="large" />,
+  },
+  {
+    label: "github-icon",
+    href: GITHUB,
+    icon: <GitHubIcon fontSize="large" />,
+  },
+];
+
 export const LinkBlock = () => {
   return (
     <Box>
       <Paper sx={{ p: 2, fontSize: "2rem" }}>
-        <Stack direction="row" justifyContent="space-between">
-          <IconButton aria-label="kakao-icon" color="primary" sx={{ width: "30%" }} href={OPEN_KAKAO} target="_blank">
-            <SvgIcon fontSize="large">
-              <KakaoTalkIcon />
-            </SvgIcon>
-          </IconButton>
-          <IconButton aria-label="email-icon" color="primary" sx={{ width: "30%" }} href={INSTAGRAM} target="_blank">
-            <InstagramIcon fontSize="large" />
-          </IconButton>
-          <IconButton aria-label="github-icon" color="primary" sx={{ width: "30%" }} href={GITHUB} target="_blank">
-            <GitHubIcon fontSize="large" />
-          </IconButton>
+        <Stack direction="row" justifyContent="space-around">
+          {LINKS.map(({ label, href, icon }) => (
+            <IconButton key={label} aria-label={label} color="primary" href={href} target="_blank">
+              {icon}
+            </IconButton>
+          ))}
         </Stack>
       </Paper>
     </Box>
