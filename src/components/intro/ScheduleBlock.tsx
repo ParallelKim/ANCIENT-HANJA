3afeacc6a6a69a5f505e84f3240d97871092953a
parallel <parallelkim12@gmail.com>
2023-11-07 21:50:17 +0900

    Refactor: Manage schedules as obj

---
+++ src/components/intro/ScheduleBlock.tsx
@@ -0,0 +1,30 @@
+import { Box, Link, Paper, Typography } from "@mui/material";
+import { ScheduleCountdown } from "./ScheduleCountdown";
+import { nearest } from "../../constants/schedules";
+
+export const ScheduleBlock = () => {
+  return (
+    <Box>
+      <Paper sx={{ p: 1, fontSize: { md: "3rem", xs: "2rem" } }}>
+        <Typography variant="h5" p={1} fontWeight={700}>
+          {nearest.eventName}까지
+        </Typography>
+        <ScheduleCountdown />
+        <Typography variant="h6" mt={1} p={1} fontWeight={700} textAlign="center">
+          <Link
+            underline="hover"
+            target="_blank"
+            href="https://calendar.google.com/calendar/u/0/r?cid=YW5jaWVudC5oYW5qYUBnbWFpbC5jb20"
+          >
+            구글 캘린더 추가하기
+          </Link>
+        </Typography>
+        <Typography textAlign="center">
+          <Link p={1} underline="hover" href="/notice#일정 알림에 관한 공지" fontSize="1rem">
+            일정 알림에 관한 공지
+          </Link>
+        </Typography>
+      </Paper>
+    </Box>
+  );
+};
--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,22 @@ export const shuffleArray = <T>(array: T[]): T[] => {
 
   return temp;
 };
+
+export const scheduler = (targetDate: string, callback: () => void) => {
+  const year = Number(targetDate.substring(0, 4));
+  const month = Number(targetDate.substring(4, 6));
+  const day = Number(targetDate.substring(6, 8));
+  const time = Number(targetDate.substring(8, 10));
+  const minute = Number(targetDate.substring(10, 12));
+  const second = Number(targetDate.substring(12, 14));
+
+  const oprDate = new Date(year, month - 1, day, time, minute, second);
+  const nowDate = new Date();
+
+  const timer = oprDate.getTime() - nowDate.getTime();
+  if (timer >= 0) {
+    setTimeout(callback, timer);
+  } else {
+    callback();
+  }
+};
