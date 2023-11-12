e2fd44c67411ff326ed901109f67629d681161a2
parallel <parallelkim12@gamil.com>
2023-11-13 00:30:26 +0900

    Fix: Input idx + 1

--- src/components/test/current/CheckAnswer.tsx
+++ src/components/test/current/CheckAnswer.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Input, Paper, Stack } from "@mui/material";
 import { useAtomValue, useSetAtom } from "jotai";
-import { answerableAtom, inputAnswerAtom } from "../../../stores/test";
+import { answerableAtom, currentUserAnswerAtom, inputAnswerAtom } from "../../../stores/test";
 
 const NUM_ICON = ["①", "②", "③", "④"];
 
@@ -8,23 +8,38 @@ export const CheckAnswer = () => {
   const answerable = useAtomValue(answerableAtom);
   const inputAnswer = useSetAtom(inputAnswerAtom);
 
+  const currentUserAnswer = useAtomValue(currentUserAnswerAtom);
+
   return (
     <Box p={2}>
       <Paper sx={{ p: 2 }}>
         {answerable ? (
           <Stack direction="row" flexWrap="wrap" useFlexGap spacing={2}>
-            {answerable.map((ans, idx) => (
-              <Button
-                variant="outlined"
-                sx={{ p: 0, width: ans.length > 5 ? "80%" : "40%", flexGrow: 1 }}
-                key={ans}
-                onClick={() => {
-                  inputAnswer(idx);
-                }}
-              >
-                {NUM_ICON[idx]} {ans}
-              </Button>
-            ))}
+            {answerable.map((ans, idx) =>
+              currentUserAnswer === idx + 1 ? (
+                <Button
+                  variant="contained"
+                  sx={{ p: 0, width: ans.length > 5 ? "80%" : "40%", flexGrow: 1 }}
+                  key={ans}
+                  onClick={() => {
+                    inputAnswer(idx + 1);
+                  }}
+                >
+                  {NUM_ICON[idx]} {ans}
+                </Button>
+              ) : (
+                <Button
+                  variant="outlined"
+                  sx={{ p: 0, width: ans.length > 5 ? "80%" : "40%", flexGrow: 1 }}
+                  key={ans}
+                  onClick={() => {
+                    inputAnswer(idx + 1);
+                  }}
+                >
+                  {NUM_ICON[idx]} {ans}
+                </Button>
+              ),
+            )}
           </Stack>
         ) : (
           <Input fullWidth />
