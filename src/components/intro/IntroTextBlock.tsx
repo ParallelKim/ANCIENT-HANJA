65b2f0e0e62727cee4ef1845faac8caf5b59240d
Parallel <parallelkim12@gamil.com>
2023-12-25 21:19:10 +0900

    Update: Intro text

--- src/components/intro/IntroTextBlock.tsx
+++ src/components/intro/IntroTextBlock.tsx
@@ -8,13 +8,14 @@ export const IntroTextBlock = () => {
           고대한자(베타)
         </Typography>
         {/* <Typography variant="h6">업데이트 내역</Typography> */}
-        <Typography>다들 시험 잘 보셨나요?</Typography>
+        <Typography>안녕하세요 오랜만입니다.</Typography>
+        <Typography>요새 너무 바빠서 일정 업데이트를 빨리 못해드렸네요</Typography>
         <p />
-        <Typography>내일은 한자 시험 결과 발표일입니다</Typography>
-        <Typography>저는 통과 못하지 싶어요 ㅋㅋㅋ</Typography>
+        <Typography>역시나 저번 시험에 떨어져서 한 번 더 봐야 하는 상황인데</Typography>
+        <Typography>당분간은 바쁠 것 같아 이번 시험은 패스하려고요</Typography>
         <p />
-        <Typography>주말 잘 보내시고 내일 아침 9시에 좋은 결과 확인하시면 좋겠네요</Typography>
-        <Typography>그동안 부족한 서비스 이용해주셔서 감사드립니다</Typography>
+        <Typography>여유 생기면 틈틈히 사이트는 업데이트 하겠습니다</Typography>
+        <Typography>날이 많이 추운데 감기 조심하시고 행복한 연말 보내세용</Typography>
         <p />
         <Typography>모든 문의 환영입니다 아래 링크로 편하게 연락 주세요</Typography>
       </Paper>
